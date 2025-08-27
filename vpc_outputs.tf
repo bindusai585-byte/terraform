732747d8a616608eed58b4c73eec8ab82fbe0bdc
bindusai585-byte <bindusai585@gmail.com>
2025-08-27 12:24:13 -0400

    create vpc

--- vpc_outputs.tf
+++ vpc_outputs.tf
@@ -7,3 +7,15 @@ output "vpc_cidr" {
   description = "value of the VPC CIDR"
   value       = var.vpc_cidr
 }
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+output "region" {
+  description = "The AWS region to deploy resources in"
+  value       = var.region
+}
