c839c703bfb52f9853c8128a9fe71b6eea6c1eb2
bindusai585-byte <bindusai585@gmail.com>
2025-08-27 12:30:29 -0400

    Update ec2_bastionhost.tf

--- ec2_bastionhost.tf
+++ ec2_bastionhost.tf
@@ -4,11 +4,13 @@ module "public_instance" {
     depends_on = [ module.vpc ]
 
   instance_type = "t3.micro"
-  key_name      = "user1"
+  key_name      = "ec2key"
   monitoring    = true
   subnet_id     = module.vpc.public_subnets[0]
-  ami           = "ami-0c55b159cbfafe1f0"
-
+  ami           = "ami-00ca32bbc84273381"
+  user_data = file("${path.module}/install_nginx.sh")
+  vpc_security_group_ids = [module.public_security-group.security_group_id]
+  #user_data = file("${path.module}/install_nginx.sh")
     tags = {
         Name = "BastionHost"
     }
